d7d0ca833f67f0066b7b366efa2a5d4933ac92ad
Farah Student <farah.gus@hotmail.com>
2019-03-26 20:18:32 -0400

    commited

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BookmarkList from './BookmarkList/BookmarkList';
 import Nav from './Nav/Nav';
 import config from './config';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 const bookmarks = [
   // {
@@ -31,20 +32,14 @@ const bookmarks = [
 
 class App extends Component {
   state = {
-    page: 'list',
     bookmarks,
     error: null,
   };
-
-  changePage = (page) => {
-    this.setState({ page })
-  }
 
   setBookmarks = bookmarks => {
     this.setState({
       bookmarks,
       error: null,
-      page: 'list',
     })
   }
 
@@ -73,23 +68,28 @@ class App extends Component {
   }
 
   render() {
-    const { page, bookmarks } = this.state
+    const { bookmarks } = this.state
     return (
       <main className='App'>
         <h1>Bookmarks!</h1>
-        <Nav clickPage={this.changePage} />
+        <Nav />
         <div className='content' aria-live='polite'>
-          {page === 'add' && (
-            <AddBookmark
+          <Route
+            path ='/add-bookmark'
+            render={({ history }) => { 
+            return <AddBookmark
               onAddBookmark={this.addBookmark}
-              onClickCancel={() => this.changePage('list')}
+              onClickCancel={() => history.push('/')}
+            />}}
             />
-          )}
-          {page === 'list' && (
-            <BookmarkList
-              bookmarks={bookmarks}
-            />
-          )}
+            <Route 
+              exact 
+              path='/'
+              render={() => 
+                <BookmarkList 
+                  bookmarks={bookmarks}
+                />}
+              />
         </div>
       </main>
     );
